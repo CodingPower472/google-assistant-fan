a3968aecb1cfab376654faafad1d21c81099ac78
Luke Bousfield <luke.j.bousfield@gmail.com>
2019-07-03 18:45:57 -0600

    Changed

--- main.js
+++ main.js
@@ -1,16 +1,31 @@
 
+const gpioMan = require('./gpio-man');
 const express = require('express');
 const bodyParser = require('body-parser');
 
 const app = express();
 
-app.use(bodyParser.json());
+let power = 2, on = false;
+
+function setPower(newPower) {
+    console.log(`Setting fan power to ${newPower}`);
+    if (newPower === 0) {
+        on = false;
+        gpioMan.allOff();
+    } else {
+        power = newPower;
+        gpioMan.setPower(power);
+    }
+}
 
 app.post('/', (req, res) => {
-  console.log('POST request');
-  console.log(req.body);
-  console.log(`Response ID: ${req.body.responseId}`);
-  console.log(`Intent: ${req.body.queryResult.intent.displayName}`);
+    console.log('POST request to root');
+    console.log(req.body);
+    console.log(`Response ID: ${req.body.responseId}`);
+    let queryResult = req.body.queryResult;
+    console.log(`Intent: ${queryResult.intent.displayName}`);
+    let intentId = queryResult.intent.name;
+    console.log(`Intent ID: ${intentId}`);
 });
 
 app.listen(8080);
