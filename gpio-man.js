bb6427508ba55691ee4928a6ecdb93701660375f
Luke Bousfield <tech.master.champ@gmail.com>
2019-07-05 12:08:57 -0600

    Changed gpio pins

--- gpio-man.js
+++ gpio-man.js
@@ -1,13 +1,14 @@
 
 const FAN_POWER_PINS = {
-    1: 4,
+    1: 3,
     2: 5,
-    3: 6
+    3: 7
 };
 
 const rpiGpio = require('rpi-gpio');
 
 for (key in FAN_POWER_PINS) {
+    console.log(`Setting pin ${FAN_POWER_PINS[key]} to output`);
     rpiGpio.setup(FAN_POWER_PINS[key]); // defaults to output
 }
 
@@ -16,10 +17,12 @@ console.log(FAN_POWER_PINS);
 module.exports = {
     setPower: function(newPower) {
         this.allOff();
+        console.log(`Sending HIGH to ${FAN_POWER_PINS[newPower]}`);
         rpiGpio.write(FAN_POWER_PINS[newPower], true);
     },
     allOff: function() {
         for (key in FAN_POWER_PINS) {
+            console.log(`Sending LOW to ${FAN_POWER_PINS[key]}`);
             rpiGpio.write(FAN_POWER_PINS[key], false);
         }
     }
