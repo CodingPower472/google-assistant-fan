eb0b21cafe4173324d9ceb1ecc7428e41db8baf4
Luke Bousfield <tech.master.champ@gmail.com>
2019-08-06 23:37:46 -0600

    Fixed gpio

--- gpio-man.js
+++ gpio-man.js
@@ -9,7 +9,7 @@ const rpiGpio = require('rpi-gpio');
 
 for (key in FAN_POWER_PINS) {
     console.log(`Setting pin ${FAN_POWER_PINS[key]} to output`);
-    rpiGpio.setup(FAN_POWER_PINS[key]); // defaults to output
+    rpiGpio.setup(FAN_POWER_PINS[key], rpiGpio.DIR_HIGH);
 }
 
 console.log(FAN_POWER_PINS);
@@ -18,12 +18,12 @@ module.exports = {
     setPower: function(newPower) {
         this.allOff();
         console.log(`Sending HIGH to ${FAN_POWER_PINS[newPower]}`);
-        rpiGpio.write(FAN_POWER_PINS[newPower], true);
+        rpiGpio.write(FAN_POWER_PINS[newPower], false);
     },
     allOff: function() {
         for (key in FAN_POWER_PINS) {
             console.log(`Sending LOW to ${FAN_POWER_PINS[key]}`);
-            rpiGpio.write(FAN_POWER_PINS[key], false);
+            rpiGpio.write(FAN_POWER_PINS[key], true);
         }
     }
 }
